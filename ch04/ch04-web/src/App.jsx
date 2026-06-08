b00da58ecff7715a519f9fe655a6b0057358033f
code1218 <skjil1218@gmail.com>
2026-06-08 14:54:42 +0900

    signup 작업중

--- ch04/ch04-web/src/App.jsx
+++ ch04/ch04-web/src/App.jsx
@@ -1,5 +1,6 @@
 import { Route, Routes } from "react-router";
 import SignUp from "./pages/SignUp";
+import SignIn from "./pages/SignIn";
 import ProtectedRoutes, { PublicOnlyRoute } from "./components/ProtectedRoutes";
 
 function App() {
@@ -8,13 +9,13 @@ function App() {
         <Routes>
             <Route path="/" element={<></>} />
 
-            <Route element={<ProtectedRoutes />}>
+            <Route element={<PublicOnlyRoute />}>
               <Route path="/auth/signup" element={<SignUp />} />
               <Route path="/auth/signin" element={<SignIn />} />
             </Route>
 
-            <Route element={<PublicOnlyRoute />}>
-              
+            <Route element={<ProtectedRoutes />}>
+              <Route path="/dash" element={<></>} />
             </Route>
         </Routes>
     )
--- ch04/ch04-web/src/pages/SignIn.jsx
+++ ch04/ch04-web/src/pages/SignIn.jsx
@@ -1,8 +1,42 @@
+import { useState } from "react";
+import { Link } from "react-router";
+
 function SignIn() {
+    const emptyInputValues = {
+        username: "",
+        password: "",
+    };
+
+    const [ inputValues, setInputValues ] = useState(emptyInputValues);
+
+    const handleInputOnChange = (e) => {
+        setInputValues(prev => ({
+            ...prev,
+            [e.target.name]: e.target.value,
+        }));
+    }
+
+    const handleSignInOnClick = () => {
+
+        setInputValues(emptyInputValues);
+    }
+
 
     return (
         <>
-            
+            <h1>로그인</h1>
+            <ul>
+                <Link to={"/auth/signup"}>회원가입</Link>
+            </ul>
+            <div>
+                <input type="text" name="username" placeholder="사용자이름" value={inputValues.username} onChange={handleInputOnChange} />
+            </div>
+            <div>
+                <input type="password" name="password" placeholder="비밀번호" value={inputValues.password} onChange={handleInputOnChange} />
+            </div>
+            <div>
+                <button onClick={handleSignInOnClick}>로그인</button>
+            </div>
         </>
     )
 }
